ebb244e2590f2b6ce3f8157d967f7b14c7f9c192
Vahid MohammadZadeh <vmohammadzadeh1@gmail.com>
2024-05-19 13:39:22 +0330

    slight changes on data loader to have train, val, and test subsets

--- data_loader.py
+++ data_loader.py
@@ -1,36 +1,35 @@
 from torchvision.datasets import FashionMNIST
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
+from torch.utils.data import random_split
 
 
 def load_data_and_transformation(batch_size):
-    transformation = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, ), (0.5, ))
-        ]
-    )
-
-    training_set = FashionMNIST(
+    train_set = FashionMNIST(
         root="data",
         train=True,
         download=True,
-        transform=transformation
+        transform=transforms.ToTensor()
     )
-    validation_set = FashionMNIST(
+    val_set = FashionMNIST(
         root="data",
         download=True,
         train=False,
-        transform=transformation
+        transform=transforms.ToTensor()
     )
 
-    train_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
+    train_subset, test_subset = random_split(
+        train_set, [0.8, 0.2]
+    )
 
-    return train_loader, val_loader, transformation
+    train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=True)
+
+    return train_loader, val_loader, test_loader
 
 
 if __name__ == "__main__":
-    tr_loader, v_loader, tr = load_data_and_transformation(16)
-    for x, y in tr_loader:
+    train_loader, val_loader, test_loader = load_data_and_transformation(16)
+    for x, y in train_loader:
         print(x.shape, y.shape)
