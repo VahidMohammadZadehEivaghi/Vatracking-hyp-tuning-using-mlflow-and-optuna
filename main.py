e89bdb90fcae3e0e4bb12a734a169975539b6adb
vahid <vmohammadzadeh1@gmail.com>
2024-06-15 16:53:16 +0330

    final model registry

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from objective import objective
 import optuna
 import mlflow
+from train import train_and_evaluate_for_one_epoch
+from model import MnistClassifier
+from data_loader import load_data
+import torch
+from torchmetrics import Accuracy
+from mlflow.types import Schema, TensorSpec
+from mlflow.models import ModelSignature
+import numpy as np
 
 if __name__ == "__main__":
 
@@ -11,3 +19,36 @@ if __name__ == "__main__":
         study = optuna.create_study(direction="maximize")
         study.optimize(objective, n_trials=20)
 
+        best_params = study.best_params
+        mlflow.log_params(best_params)
+        epoch = 50
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        batch_size = 16
+        train_loader, val_loader, test_loader = load_data(batch_size)
+        model = MnistClassifier().to(device)
+        metric_fn = Accuracy(task="multiclass", num_classes=10).to(device)
+        loss_fn = torch.nn.CrossEntropyLoss()
+
+        optimizer = torch.optim.SGD(model.parameters(), **best_params)
+
+        for e in range(1, epoch + 1):
+            print(f"------------------Epoch {e} / {epoch}-------------------")
+            acc = train_and_evaluate_for_one_epoch(
+                train_loader,
+                val_loader,
+                model,
+                optimizer,
+                metric_fn,
+                loss_fn,
+                device
+            )
+            print(f"acc:{acc}")
+
+        input_schema = Schema([TensorSpec(np.dtype(np.float32), (-1, 28, 28))])
+        output_schema = Schema([TensorSpec(np.dtype(np.float32), (-1, 10))])
+        signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+        mlflow.pytorch.log_models(
+            model,
+            "mnist-classifier",
+            signature=signature
+        )
